084efde5aa56667700ac53a5a3fb56132d831a97
Alan Reis <alanreisanjo@gmail.com>
2023-05-28 21:08:50 -0300

    Tentativa

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,13 @@ import Head from "next/head";
 import "../public/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
-    return (
-        <>
-            <Head>
-                <title>Pokédex</title>
-                <link rel="icon" href="/favicon.ico" /> 
-            </Head>
-            <Component {...pageProps} />;
-        </>
-    );
+	return (
+		<>
+			<Head>
+				<title>Pokédex</title>
+				<link rel="icon" href="/favicon.ico" /> 
+			</Head>
+			<Component {...pageProps} />;
+		</>
+	);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,55 +2,55 @@ import React, { useEffect, useState } from 'react';
 import * as Component from '../public/components';
 
 function CapitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+	return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 async function setData(urlPool, stateFunction) {
-    const newDataArray = []; // Array temporário para armazenar os novos dados
+	const newDataArray = []; // Array temporário para armazenar os novos dados
   
-    for (const url of urlPool) {
-      const response = await fetch(url);
-      const pokemonData = await response.json();
+	for (const url of urlPool) {
+	  const response = await fetch(url);
+	  const pokemonData = await response.json();
   
-      if (pokemonData.is_default) {
-        const newData = (
-          <Component.Card
-            pokemonId={pokemonData.id}
-            pokemonName={CapitalizeFirstLetter(pokemonData.name)}
-            pokemonSprite={pokemonData.sprites.other['official-artwork'].front_default}
-            pokemonTypes={pokemonData.types}
-            key={pokemonData.id}
-          />
-        );
+	  if (pokemonData.is_default) {
+		const newData = (
+		  <Component.Card
+			pokemonId={pokemonData.id}
+			pokemonName={CapitalizeFirstLetter(pokemonData.name)}
+			pokemonSprite={pokemonData.sprites.other['official-artwork'].front_default}
+			pokemonTypes={pokemonData.types}
+			key={pokemonData.id}
+		  />
+		);
   
-        newDataArray.push(newData);
-        stateFunction(state => [...newDataArray]);
-      }
-    }
+		newDataArray.push(newData);
+		stateFunction(state => [...newDataArray]);
+	  }
+	}
 }
 
 export default function Index() {
-    let toFetchPool = []; 
-    let [pokedex, setPokedex] = useState([]);
+	let toFetchPool = []; 
+	let [pokedex, setPokedex] = useState([]);
 
-    useEffect(() => {
-        fetch('/api').then((data) =>  data.json()).then((value) => {
+	useEffect(() => {
+		fetch('/api').then((data) =>  data.json()).then((value) => {
 
-            value.results.forEach((element) => {
-                toFetchPool.push(element.url);
-            });
-            
-            setData(toFetchPool, setPokedex);
+			value.results.forEach((element) => {
+				toFetchPool.push(element.url);
+			});
+			
+			setData(toFetchPool, setPokedex);
 
-        });
-    }, []);
-        
-    return (
-        <>
+		});
+	}, []);
+		
+	return (
+		<>
 			<Component.Navbar />
 			<div className="pokedex-container">
 				{pokedex}
 			</div>
-        </>
-    )
+		</>
+	)
 }
